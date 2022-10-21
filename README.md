b90f43b3f4e17162a7e5f2b6f981e303a2a4c441
kevin_garay <kgaray@applaudostudios.dev>
2022-10-21 15:59:48 -0500

    added UserStock resource

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This is the finance tracker app from the Complete Ruby on Rails Developer course
 
+* Using devise
+* Use of IEX cloud API for stock info
+* Render JavaScript response to search
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,12 @@
 
 # model stock
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  
+  validates :name, :ticker, presence: true
+
+  
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: ENV['EIX_PUBLISHABLE'],
