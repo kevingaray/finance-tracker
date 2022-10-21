ebdfeeafeb47be135c1229b229f313f2b580de07
kevin_garay <kgaray@applaudostudios.dev>
2022-10-21 11:08:02 -0500

    Display stock objects

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,13 @@ class Stock < ApplicationRecord
       secret_token: ENV['EIX_SECRET'],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
-    client.quote(ticker_symbol).latest_price
+
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    rescue => exception
+      return nil
+    end
+
   end
 
 end
