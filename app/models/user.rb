a5c69ec1aa0eaa534db6d182b8a4e5a4699cad27
kevin_garay <kgaray@applaudostudios.dev>
2022-10-24 09:03:17 -0500

    Set friendships backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@
 
 # class user with devise methods
 class User < ApplicationRecord
+  # stocks
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  # friend
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
