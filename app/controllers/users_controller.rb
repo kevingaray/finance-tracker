1d702a4c6c1fbc0fddce0f00cff7f5ec0fa6daed
kevin_garay <kgaray@applaudostudios.dev>
2022-10-24 12:06:35 -0500

    display user/friend profile and tracking options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,17 @@
 # add user methods to devise
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
   def my_friends
     @friends = current_user.friends
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
 
